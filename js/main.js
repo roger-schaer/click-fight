e2d3417d31c5a33a875b00a7394816bf5fcf03dd
Roger Schaer <roger.schaer@hevs.ch>
2017-04-08 23:43:31 +0200

    Fix POST

--- js/main.js
+++ js/main.js
@@ -21,13 +21,13 @@ function startTimer(duration, display) {
             $("#congrats").show();
 
             $.post('https://api.telegram.org/bot349061307:AAH2s3gMeu-LFIoNE-fVWvlgK1pfLUjYlQY/setGameScore?user_id=' + getParameterByName('userId')
-                + '&score=' + parseInt($("#score").text()
+                + '&score=' + parseInt($("#score").text())
                 + '&inline_message_id=' + getParameterByName('inlineMessageId')
-                ), function(data){
-                console.log(data);
-            });
+                , function(data){
+                    console.log(data);
+                });
         }
-    }, 1000);
+    }, 100);
 }
 
 $(function(){
